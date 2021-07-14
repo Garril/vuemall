f6bc538adcf6849b9cb351935431f390e4a671f9
Kuoc <zev20010120@163.com>
2021-07-14 18:06:00 +0800

    'InitializeFilesDetail'

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      //extensions: [], // 配置后缀,这里略
+      alias: { // @配置好了，为src
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        // 'router': '@/router', router和store不需要了，$router和$store直接引用对象
+      }
+    }
+  }
+}
