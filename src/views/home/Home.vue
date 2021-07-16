f525071e522f3a147d1f6f8616dc6f006ef7191f
Kuoc <zev20010120@163.com>
2021-07-16 17:28:25 +0800

    'QuestionInSwipe'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,45 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav-bar">
-      <div slot="nav-bar-center">购物街</div>
-    </nav-bar>
+    <nav-bar class="home-nav-bar"> <div slot="nav-bar-center">购物街</div> </nav-bar>
+    <!-- 这里的轮播图，有一个问题，有时候需要刷新，存在加载后，轮播图不会动的问题 -->
+    <home-swiper :banners="this.banners"/>
+
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar.vue'
+  import NavBar from 'components/common/navbar/NavBar.vue';
+  import HomeSwiper from './childComps/HomeSwiper.vue'
+
+  import {getHomeMultidata} from 'network/home';
+
 
   export default {
     name:'Home',
     components:{
       NavBar,
+      HomeSwiper,
+
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      this.getHomeMultidata();
+
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then((res) => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })        
+      }
     }
+
   }
 </script>
 
